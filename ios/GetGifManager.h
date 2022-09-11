3d43678e3a5729d58152c9d30ea87659ef1c8861
Celia <111430976+CeliaHrrferreira@users.noreply.github.com>
2022-09-11 02:07:28 +0200

    DEVELOP: iOS native module

---
+++ ios/GetGifManager.h
@@ -0,0 +1,14 @@
+//
+//  GetGifManager.h
+//  CatpediaReact
+//
+//  Created by Celia Herrera Ferreira on 11/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <React/RCTBridgeModule.h>
+
+@interface SPGetGifManager : NSObject <RCTBridgeModule>
+
+@end
